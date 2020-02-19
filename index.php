2f36fcbdfbd4f450a1e15c189f43f2f5e9ac7c22
guidi1401 <amandaguidi2003@gmail.com>
2020-02-19 17:21:14 -0300

    organizando atividades

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head> 
+		<meta charset="UTF-8"/>
+		<title>Organizando atividades WEB</title>
+	</head>
+	<body>
+		<p> <h1> Pagina com links das minhas atividades </h1> </p>
+	</body>
+</html>
