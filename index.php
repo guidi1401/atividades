c7748265476bb924bdf5ce4912148d9a30e4f450
guidi1401 <amandaguidi2003@gmail.com>
2020-09-15 09:16:00 -0300

    Engenharia Reversa

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 		<p> <h1> Pagina com links das minhas atividades </h1> </p>
 		<p> <a href="20200811/index.php"> Exercício Editor de Texto com Jquery </a> </p>
 		<p> <a href="20200819/index.php"> Exercício Engenharia Reversa </a> </p>
+		<p> <a href="20200826_individual/exercicio_ajax.php"> Exercício Ajax </a> </p>
 		<p> <a href="20200916/form.php"> Atividade - API VIACEP </a> </p>
 		<p> <a href="projeto/grupoB.php"> Projeto Prático - 1º Bimestre </a> </p>
 	</body>
