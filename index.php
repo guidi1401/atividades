bfac698b1146bf7d64faae1bb4ba366176a8d1b6
guidi1401 <amandaguidi2003@gmail.com>
2020-09-15 08:46:01 -0300

    Editor de texto com Jquery

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	</head>
 	<body>
 		<p> <h1> Pagina com links das minhas atividades </h1> </p>
+		<p> <a href="20200811/index.php"> Exercício Editor de Texto com Jquery </a> </p>
 		<p> <a href="20200819/index.php"> Exercício Engenharia Reversa </a> </p>
 		<p> <a href="20200916/form.php"> Atividade - API VIACEP </a> </p>
 		<p> <a href="projeto/grupoB.php"> Projeto Prático - 1º Bimestre </a> </p>
