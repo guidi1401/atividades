5136ade5781afd1d66dcf3b6046ac1b94d5deb5f
guidi1401 <amandaguidi2003@gmail.com>
2020-09-15 09:57:18 -0300

    Atualização index

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 		<title>Organizando atividades WEB</title>
 	</head>
 	<body>
-		<p> <h1> Pagina com links das minhas atividades </h1> </p>
-		<p> <a href="20200811/index.php"> Exercício Editor de Texto com Jquery </a> </p>
-		<p> <a href="20200819/index.php"> Engenharia Reversa </a> </p>
-		<p> <a href="20200826_individual/exercicio_ajax.php"> Exercício Ajax </a> </p>
-		<p> <a href="20200826/index.php"> Exercício Ajax</a> </p>
-		<p> <a href="20200916/form.php"> Atividade - API VIACEP </a> </p>
-		<p> <a href="projeto/grupoB.php"> Projeto Prático - 1º Bimestre </a> </p>
+		<p> <h1> Página com links das minhas atividades: </h1> </p>
+		<p> <a href="20200811/index.php"> 11/08/2020 - Exercício Editor de Texto com Jquery </a> </p>
+		<p> <a href="20200819/index.php"> 19/08/2020 - Engenharia Reversa </a> </p>
+		<p> <a href="20200826_individual/exercicio_ajax.php"> 26/08/2020 - Exercício Ajax </a> </p>
+		<p> <a href="20200826/index.php"> 26/08/2020 - Exercício Ajax</a> </p>
+		<p> <a href="20200916/form.php"> 16/09/2020 - Atividade API VIACEP </a> </p>
+		<p> <a href="projeto/grupoB.php"> 16/09/2020 - Projeto Prático 1º Bimestre </a> </p>
 	</body>
 </html>
